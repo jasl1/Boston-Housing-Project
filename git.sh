c994db854db5e97628015457b0bf1958c3f660f5
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-21 19:55:17 -0400

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,3 @@
+git init
+git add create_database.py retrieve_data.py process_data.cpp train_model.py
+git commit -m "Initial commit with database creation, data retrieval, processing, and model training scripts"
